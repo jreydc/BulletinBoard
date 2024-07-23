f698f692e64e9bfee35ec195404411a5a2bf1f28
jreydc <jreydc@gmail.com>
2024-07-23 10:12:11 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulletinBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
